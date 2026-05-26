360ee58f548f96956316241116643f32d6d56544
fukunaga19731125-cyber <fukunaga19731125@gmail.com>
2026-05-27 08:56:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+# LINE Developersから取得した値を設定してください
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
 def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
     return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="メッセージを受け取りました！")
+    )
 
 if __name__ == "__main__":
     app.run()
